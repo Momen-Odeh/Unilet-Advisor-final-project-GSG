95a91d6e1d567fdc64978833fee4db62e939f73a
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-05-09 18:43:08 +0300

    single Card done

---
+++ src/Components/CardAction/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import home from './images/home.jpg'
+
+
+import { createUseStyles } from 'react-jss';
+import DarkTitle from '../DarkTitle';
+import RateContainer from '../RateContainer';
+const useStyles  = createUseStyles({
+    Card:{
+        width:"20rem",
+    }
+    
+  })
+const CardAction = ({title,img,value,numRated,clickOperation}) => {
+    const classes = useStyles ();
+  return (
+    <Card className={classes.Card}>
+      <Card.Img variant="top" src={home} />
+      <Card.Body>
+        <Card.Title> <DarkTitle text={title}/></Card.Title>
+        <Card.Text>
+        <RateContainer value={value} numRated={numRated} clickOperation={clickOperation}/>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default CardAction
--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ButtonAction from '../../Components/ButtonAction'
-
+import CardAction from '../../Components/CardAction'
 const HomePage = () => {
   return (
     <>
       <div>HomePage</div>
       <ButtonAction text="Log In" arrow dark bold/>
+      <CardAction key={2} title="123 That Road Bournemouth" value={2.9} numRated={59}/>
     </>
     
   )
