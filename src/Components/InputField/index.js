174439479ab94e3b5b5cb7268d634ec60845c909
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-05-10 01:40:51 +0300

    input filed sone

---
+++ src/Components/InputField/index.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { createUseStyles } from 'react-jss';
+import userIcon from './images/usrer.png'
+import EmailIcon from './images/emailIcon.png'
+import passwordIcon from './images/pass.png'
+const useStyles  = createUseStyles({
+    Form:{
+        width:"90%",
+        margin:"0 auto",
+    },
+    InputGroup:{
+        border:"1px solid #D9D9D9",
+        borderRadius:"5px",
+        borderLeft:"5px solid #3C3C3B",
+    },
+    Control:{
+        border:"none",
+        fontFamily:"'Open Sans', sans-serif",
+        fontWeight:"400",
+        fontSize:"17px",
+        lineHeight:"23.15px",
+        "&:focus":{
+            boxShadow:"none"
+        }
+    },
+    Text:{
+        backgroundColor:"white",
+        border:"none",
+    },
+    icon:{
+        width:"30px",
+        height:"30px"
+    },
+})
+const InputField = ({type}) => {
+    const classes = useStyles ();
+    let data
+    if(type === "user")
+    {
+        data={
+            placeholder:"Full name here",
+            icon:userIcon,
+            type:"text"
+        }
+    }
+    else if(type === "email")
+    {
+        data={
+            placeholder:"Enter your email address",
+            icon:EmailIcon,
+            type:"email"
+        }
+    }
+    else if(type === "password")
+    {
+        data={
+            placeholder:"Enter your password",
+            icon:passwordIcon,
+            type:"password"
+        }
+    }
+    else if(type === "confirmPassword")
+    {
+        data={
+            placeholder:"Conform password",
+            icon:passwordIcon,
+            type:"password"
+        }
+    }
+  return (
+    <Form className={classes.Form}>
+        <Form.Group>
+            <InputGroup className={classes.InputGroup}>
+                <InputGroup.Text className={classes.Text}><img src={data.icon} alt="icon" className={classes.icon}/></InputGroup.Text>
+                <Form.Control className={classes.Control} type={data.type} placeholder={data.placeholder} />
+            </InputGroup>
+        </Form.Group>
+    </Form>
+
+  )
+}
+
+export default InputField
--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ButtonAction from '../../Components/ButtonAction'
+import InputField from '../../Components/InputField'
 
 const HomePage = () => {
   return (
     <>
-      <div>HomePage</div>
-      <ButtonAction text="Log In" arrow dark bold/>
+      {/* <div>HomePage</div>
+      <ButtonAction text="Log In" arrow dark bold/> */}
+      <InputField type={"user"}/>
+      <InputField type={"email"}/>
+      <InputField type={"password"}/>
+      <InputField type={"confirmPassword"}/>
     </>
     
   )
