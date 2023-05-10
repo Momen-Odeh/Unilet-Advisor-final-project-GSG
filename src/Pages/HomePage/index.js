64d90bcaff476c1e114cd2e0572919675dc8f9e5
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-05-10 22:38:04 +0300

    make home page clean

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import ButtonAction from '../../Components/ButtonAction'
-import SearchInput from '../../Components/SearchInput'
-import SortResult from '../../Components/SortResult'
-
 
 const HomePage = () => {
   return (
     <>
-      <InputField type={"user"}/>
-      <InputField type={"email"}/>
-      <InputField type={"password"}/>
-      <InputField type={"confirmPassword"}/>
     </>
     
   )
