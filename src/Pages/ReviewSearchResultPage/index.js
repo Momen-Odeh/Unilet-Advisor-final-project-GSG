b59eb5a52d6cedf33828770700c9fd1970b6bbfd
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-05-12 22:19:37 +0300

    result search done

--- src/Pages/ReviewSearchResultPage/index.js
+++ src/Pages/ReviewSearchResultPage/index.js
@@ -8,21 +8,35 @@ import Cards from '../../Components/Cards';
 import ButtonAction from '../../Components/ButtonAction';
 import SearchInput from '../../Components/SearchInput';
 import SortResult from '../../Components/SortResult';
-
+import {createUseStyles} from 'react-jss'
+const useStyle = createUseStyles({
+  RowText:{
+    justifyContent:"space-between",
+    alignItems:"flex-end",
+    gap:"20px"
+  },
+  ColText:{
+    marginBottom:"10px",
+  },
+})
 const ReviewSearchResultPage = () => {
+  const clasess = useStyle()
+
   return (
     <Container>
-        <Row className='mb-3'>
+        <Row className='mb-5'>
             <Col className='text-center'>
                 <PrimaryTitle text="Reviews properties" sizeText={40} space={1} lineHeight={47}/>
             </Col>
         </Row>
-        <Row className='mb-5'>
-            <Col className='' xs={5}>
-                <SecondaryText text="Search for a specific UK address or town" weightText={400} lineHeight={23.15} sizeText={17}/>
+        <Row className={`${clasess.RowText} mb-5`}>
+            <Col className='' xl={5} lg={6} md={7} xs={12}>
+                <div className={clasess.ColText}>
+                  <SecondaryText  text="Search for a specific UK address or town" weightText={400} lineHeight={23.15} sizeText={17}/>
+                </div>
                 <SearchInput btnText="Search" placeHolder="Start typing the address of the property"/>
             </Col>
-            <Col className='text-center'  xs={3}>
+            <Col lg={3} md={3} xs={12}>
                 <SortResult/>
             </Col>
         </Row>
@@ -31,9 +45,9 @@ const ReviewSearchResultPage = () => {
                 <Cards/>
             </Col>
         </Row>
-        <Row>
-            <Col className='text-center'>
-                <ButtonAction text="View All" dark bold/>
+        <Row className='justify-content-center'>
+            <Col className='' xs={"auto"}>
+              {/* Pagination code here */}
             </Col>
         </Row>
     </Container>
