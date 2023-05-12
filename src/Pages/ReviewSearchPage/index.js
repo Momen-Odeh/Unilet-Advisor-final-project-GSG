aba39a163f04b7b9b7c7f6129b6e3189dbfea2ab
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-05-12 18:49:00 +0300

    Search hero page

--- src/Pages/ReviewSearchPage/index.js
+++ src/Pages/ReviewSearchPage/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import SearchHero from '../../Components/SearchHero';
 const ReviewSearchPage = () => {
   return (
-    <div>ReviewSearchPage</div>
+    <>
+    <SearchHero/>
+    </>
   )
 }
 
