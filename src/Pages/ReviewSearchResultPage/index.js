59e412ad4923336f7748b502415b24bd8bfbe55e
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-05-21 02:41:30 +0300

    sort integration done

--- src/Pages/ReviewSearchResultPage/index.js
+++ src/Pages/ReviewSearchResultPage/index.js
@@ -25,7 +25,8 @@ const ReviewSearchResultPage = () => {
   const [places, setPlaces] = useState([]);
   const {title} = useParams("title")
   const navigate = useNavigate()
-  const [searchText,setSearchText]  = useState("")
+  const [searchText,setSearchText] = useState("")
+  const [sortText,setSortText] = useState("Most Recent")
   useEffect(()=>{
       RetriveData("Places").then((resp)=>{
         if(title === "ALL")
@@ -45,7 +46,42 @@ const ReviewSearchResultPage = () => {
           
       })
   },[title])
-
+    function findTotal(place) {
+        const {star1,star2,star3,star4,star5,} = place.AverageRating
+        return star1+star2+star3+star4+star5
+    }
+    function findAvg(place) {
+        let len = findTotal(place)
+        if(len === 0) return 0 
+        const {star1,star2,star3,star4,star5,} = place.AverageRating
+        return ((1*star1+2*star2+3*star3+4*star4+5*star5)/len).toFixed(1)
+    }
+  useEffect(()=>{
+    if(sortText === "Most Recent")
+    {
+      const sortedPlaces = [...places].sort((a, b) => {
+        if (a.publishDate > b.publishDate) {
+          return -1;
+        } else if (a.publishDate < b.publishDate) {
+          return 1;
+        }
+        return 0;
+      });
+      setPlaces(sortedPlaces);
+    }
+    else if(sortText === "Top Rated")
+    {
+      const sortedPlaces = [...places].sort((a, b) => {
+        if (findAvg(a) > findAvg(b)) {
+          return -1;
+        } else if (findAvg(a) < findAvg(b)) {
+          return 1;
+        }
+        return 0;
+      });
+      setPlaces(sortedPlaces);
+    }
+  },[sortText])
   function handleSearchBtn(){
     let val = searchText.trim()
     if(val.length === 0)
@@ -72,7 +108,7 @@ const ReviewSearchResultPage = () => {
                 <SearchInput btnText="Search" placeHolder="Start typing the address of the property" txetHandel={{searchText,setSearchText}} onClick={handleSearchBtn}/>
             </Col>
             <Col lg={3} md={3} xs={12}>
-                <SortResult/>
+                <SortResult handleSort={{sortText,setSortText}}/>
             </Col>
         </Row>
         <Row className='mb-4'>
