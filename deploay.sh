94161e7b8e4c621afa67cb6b36dbba4e52b52e63
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-06-09 19:41:42 +0300

    uplode deploy file

---
+++ deploay.sh
@@ -0,0 +1,10 @@
+echo "Switching to branch master"
+git checkout master
+
+echo "Building app..."
+npm run build
+
+echo "Deploying files to serevr... "
+scp -r build/* ubuntu@13.53.206.195:/var/www/192.53.122.115/
+
+echo "Done!"
