91188452f87e3523b9a76bf024b5a6af91188839
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-06-09 20:11:36 +0300

    update file

--- deploay.sh
+++ deploay.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to serevr... "
-scp -r build/* ubuntu@13.53.206.195:/var/www/192.53.122.115/
+scp -r build/* ubuntu@13.53.206.195:/var/www/13.53.206.195/
 
 echo "Done!"
